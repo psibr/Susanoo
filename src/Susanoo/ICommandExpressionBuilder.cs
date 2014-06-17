d81c323be726966078bbea16b13c293b24d76607
Ovan Crone <ovan@psibr.net>
2014-06-17 17:49:44 -0500

    Added method signature and stubbed implementation of DefineCommand<TResult>

--- src/Susanoo/ICommandExpressionBuilder.cs
+++ src/Susanoo/ICommandExpressionBuilder.cs
@@ -3,7 +3,7 @@
     public interface ICommandExpressionBuilder
     {
         /// <summary>
-        /// Begins the command definition process using a Fluent API implementation, move to next step with DefineMappings on the result of this call.
+        /// Begins the command definition process using a Fluent API implementation, move to next step with DefineResultMappings on the result of this call.
         /// </summary>
         /// <typeparam name="TFilter">The type of the filter.</typeparam>
         /// <typeparam name="TResult">The type of the result.</typeparam>
@@ -13,14 +13,14 @@
         ICommandExpression<TFilter, TResult> DefineCommand<TFilter, TResult>(string commandText, System.Data.CommandType commandType)
             where TResult : new();
 
-        ///// <summary>
-        ///// Begins the command definition process using a Fluent API implementation, move to next step with DefineMappings on the result of this call.
-        ///// </summary>
-        ///// <typeparam name="TResult">The type of the result.</typeparam>
-        ///// <param name="commandText">The command text.</param>
-        ///// <param name="commandType">Type of the command.</param>
-        ///// <returns>ICommandExpression&lt;TResult&gt;.</returns>
-        //ICommandExpression<TResult> DefineCommand<TResult>(string commandText, System.Data.CommandType commandType)
-        //    where TResult : new();
+        /// <summary>
+        /// Begins the command definition process using a Fluent API implementation, move to next step with DefineResultMappings on the result of this call.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="commandText">The command text.</param>
+        /// <param name="commandType">Type of the command.</param>
+        /// <returns>ICommandExpression&lt;TFilter, TResult&gt;.</returns>
+        ICommandExpression<TResult> DefineCommand<TResult>(string commandText, System.Data.CommandType commandType)
+            where TResult : new();
     }
 }
