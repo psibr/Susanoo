415461d46648b26054723ccbfd20d33045ad4270
Ovan Crone <ovan@psibr.net>
2014-06-17 17:50:49 -0500

    Added method signature and stubbed implementation of DefineCommand<TResult>

--- src/Susanoo/CommandBuilder.cs
+++ src/Susanoo/CommandBuilder.cs
@@ -15,5 +15,18 @@
         {
             return new CommandExpression<TFilter, TResult>(CommandManager.Instance.Container.Resolve<IDatabaseManager>(), commandText, commandType);
         }
+
+        /// <summary>
+        /// Begins the command definition process using a Fluent API implementation, move to next step with DefineResultMappings on the result of this call.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="commandText">The command text.</param>
+        /// <param name="commandType">Type of the command.</param>
+        /// <returns>ICommandExpression&lt;TFilter, TResult&gt;.</returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public ICommandExpression<TResult> DefineCommand<TResult>(string commandText, System.Data.CommandType commandType) where TResult : new()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
