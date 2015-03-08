cbed5896a4cbf05731b1feb1a964f4d04324f636
Ovan <ovan@psibr.net>
2015-03-07 23:35:11 -0600

    Built in type null handling, added tests

--- src/Susanoo.Json.Tests/JSON.cs
+++ src/Susanoo.Json.Tests/JSON.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
-using Susanoo.Json.Tests;
 using Susanoo.Pipeline.Command;
 
 namespace Susanoo.Json.Tests
@@ -14,30 +9,34 @@ namespace Susanoo.Json.Tests
     [TestFixture]
     public class JSON
     {
-//        [Test(Description = "Tests that results correctly map data to CLR types.")]
-//        public void KeyValueJson()
-//        {
-//            var results = CommandManager.DefineCommand<KeyValuePair<string, string>>(@"
-//                SELECT Int, String
-//                FROM (VALUES ('1', 'One'), ('2', 'Two'), ('3', 'Three'), ('4', 'Four')) AS SampleSet(Int, String)
-//                ORDER BY Int", CommandType.Text)
-//                .ExcludeProperty(o => o.Key)
-//                .ExcludeProperty(o => o.Value)
-//                .SendNullValues(NullValueMode.FilterOnlyFull)
-//                .DefineResults<KeyValuePair<string, string>>()
-//                .ForResults(expression =>
-//                {
-//                    expression.ForProperty(pair => pair.Key,
-//                        configuration => configuration.UseAlias("Int"));
-//                    expression.ForProperty(pair => pair.Value,
-//                        configuration => configuration.UseAlias("String"));
-//                })
-//                .Realize()
-//                .ExecuteToJson(Setup.DatabaseManager);
+        [Test(Description = "Tests that results correctly map data to CLR types.")]
+        public void KeyValueJson()
+        {
+            var results = CommandManager.DefineCommand<KeyValuePair<string, string>>(@"
+                    SELECT Int, String
+                    FROM (VALUES ('1', 'One'), ('2', 'Two'), ('3', 'Three'), ('4', 'Four')) AS SampleSet(Int, String)
+                    ORDER BY Int", CommandType.Text)
+                .ExcludeProperty(o => o.Key)
+                .ExcludeProperty(o => o.Value)
+                .SendNullValues(NullValueMode.FilterOnlyFull)
+                .DefineResults<KeyValuePair<string, string>>()
+                .ForResults(expression =>
+                {
+                    expression.ForProperty(pair => pair.Key,
+                        configuration => configuration.UseAlias("Int"));
 
-//            GC.KeepAlive(results);
+                    expression.ForProperty(pair => pair.Value,
+                        configuration => configuration.UseAlias("String"));
+                })
+                .Realize()
+                .ExecuteToJson(Setup.DatabaseManager);
 
-//            Assert.AreEqual(1,1);
-//        }
+            Assert.AreEqual(
+                 "[{\"Int\":\"\\\"1\\\"\",\"String\":\"\\\"One\\\"\"},"
+                + "{\"Int\":\"\\\"2\\\"\",\"String\":\"\\\"Two\\\"\"}," 
+                + "{\"Int\":\"\\\"3\\\"\",\"String\":\"\\\"Three\\\"\"},"
+                + "{\"Int\":\"\\\"4\\\"\",\"String\":\"\\\"Four\\\"\"}]", 
+                results);
+        }
     }
 }
