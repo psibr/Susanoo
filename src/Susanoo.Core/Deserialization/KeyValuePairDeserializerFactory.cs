bb9ea5cc4c7cb0eee02fa713d08c42c8a1acbf93
psibernetic <ovan@psibr.net>
2015-09-15 16:11:42 -0500

    Adding the kvp deserializer factory I missed

---
+++ src/Susanoo.Core/Deserialization/KeyValuePairDeserializerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Susanoo.Processing;
+using Susanoo.ResultSets;
+
+namespace Susanoo.Deserialization
+{
+    /// <summary>
+    /// Maps properties to a KeyValuePair using Activator
+    /// </summary>
+    public class KeyValuePairDeserializerFactory
+        : IDeserializerFactory
+    {
+        /// <summary>
+        /// Determines whether this deserializer applies to the type.
+        /// </summary>
+        /// <returns><c>true</c> if this instance can deserialize; otherwise, <c>false</c>.</returns>
+        public bool CanDeserialize(Type type)
+        {
+            return type.IsGenericType &&
+                   type.GetGenericTypeDefinition() == typeof (KeyValuePair<,>);
+        }
+
+        /// <summary>
+        /// Builds a deserializer.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="mappings">The mappings.</param>
+        /// <returns>IEnumerable&lt;TResult&gt;.</returns>
+        public Func<IDataReader, ColumnChecker, IEnumerable<TResult>> BuildDeserializer<TResult>(ICommandResultMappingExport mappings)
+        {
+            return new KeyValuePairDeserializer(mappings, typeof (TResult)).Deserialize<TResult>;
+        }
+    }
+}
