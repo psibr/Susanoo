92fd1dd83ea24f6ca3ca6754c7e5d61f50691444
psibernetic <ovan@psibr.net>
2015-09-24 10:08:51 -0500

    missed a file!

---
+++ src/Susanoo.Core/ResultSets/ICommandSingleResultExpression.cs
@@ -0,0 +1,34 @@
+﻿#region
+
+using System;
+using Susanoo.Mapping;
+using Susanoo.Processing;
+
+#endregion
+
+namespace Susanoo.ResultSets
+{
+    /// <summary>
+    /// Provides methods for customizing how results are handled and compiling result mappings.
+    /// </summary>
+    /// <typeparam name="TFilter">The type of the filter.</typeparam>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    public interface ICommandSingleResultExpression<TFilter, TResult> :
+        ICommandResultExpression<TFilter>
+    {
+        /// <summary>
+        /// Allows customization of result set.
+        /// </summary>
+        /// <param name="mappings">The mappings.</param>
+        /// <returns>ICommandSingleResultExpression&lt;TFilter, TResult&gt;.</returns>
+        ICommandSingleResultExpression<TFilter, TResult> ForResults(
+            Action<IResultMappingExpression<TFilter, TResult>> mappings);
+
+        /// <summary>
+        /// Realizes the pipeline and compiles result mappings.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>INoResultCommandProcessor&lt;TFilter, TResult&gt;.</returns>
+        ISingleResultSetCommandProcessor<TFilter, TResult> Realize(string name = null);
+    }
+}
