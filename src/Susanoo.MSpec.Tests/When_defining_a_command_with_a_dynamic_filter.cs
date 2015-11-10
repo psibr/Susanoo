009734ca0a25b0c0ee0c36c013e2d88e1c3e7fa8
Ovan Crone <ovan@psibr.net>
2015-11-10 03:12:53 -0600

    Experimenting with MSpec

---
+++ src/Susanoo.MSpec.Tests/When_defining_a_command_with_a_dynamic_filter.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Machine.Specifications;
+using Susanoo.Command;
+
+namespace Susanoo.MSpec.Tests
+{
+    [Subject(typeof(CommandManager))]
+    public class When_defining_a_command_with_a_dynamic_filter
+    {
+        static ICommandExpression<dynamic> command;
+
+        Establish context = () => { };
+
+        Because of = () => command = CommandManager.Instance.DefineCommand("SELECT 1 AS Success", CommandType.Text);
+
+        It should_have_a_type_param_of_object = () => command.ShouldBeAssignableTo(typeof(ICommandExpression<object>));
+
+        Cleanup after = () => { };
+    }
+}
